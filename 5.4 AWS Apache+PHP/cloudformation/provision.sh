3e401ded70d59314160a6115e5f9e64f2356df8c
Igor Makeev <i.makeev@a1qa.com>
2023-01-06 13:38:30 +0100

    update provision.sh

--- 5.4 AWS Apache+PHP/cloudformation/provision.sh
+++ 5.4 AWS Apache+PHP/cloudformation/provision.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -e
-
 yum -y update
+yum -y install git
 
+
+# get configs
 app_path="/var/www"
+wget https://raw.githubusercontent.com/imakeeva1qa/devops_training/main/5.4%20AWS%20Apache%2BPHP/config/wp-config.php -P /tmp/
+wget https://raw.githubusercontent.com/imakeeva1qa/devops_training/main/5.4%20AWS%20Apache%2BPHP/config/wordpress.conf -P /tmp/
+wget https://raw.githubusercontent.com/imakeeva1qa/devops_training/main/5.4%20AWS%20Apache%2BPHP/cloudformation/db.sql -P /tmp/
+
 
 # Apache
 yum install httpd -y
@@ -14,10 +20,9 @@ systemctl enable httpd.service
 
 # MySQL
 wget http://repo.mysql.com/mysql57-community-release-el7.rpm
-# yum install yum-utils -y
 rpm -ivh mysql57-community-release-el7.rpm
-yum -y update
 rpm --import http://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+yum -y update
 yum install mysql-server -y
 
 systemctl start mysqld
@@ -29,10 +34,11 @@ mysqladmin --user=root --password=${password} password ${new_password}
 mysql --user=root --password=${new_password} -e "UNINSTALL PLUGIN validate_password;"
 mysqladmin --user=root --password=${new_password} password ""
 
-mysql < /tmp/db.sql    # TODO create a file or Import
-rm /tmp/db.sql
+mysql < /tmp/db.sql
+
 
 # PHP
+yum -y update
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum makecache
@@ -42,16 +48,23 @@ amazon-linux-extras enable php8.0
 yum clean metadata
 yum install -y php-{pear,cgi,pdo,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip}
 
-# nodeJS
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-yum install nodejs -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-nvm install 16
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-npm install -g gatsby-cli 1.1.41-13
-# npm install -g gatsby-cli
-# npm install gatsby@next --legacy-peer-deps
+
+# nodeJS + Gatsby
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | bash -
+yum install -y nodejs
+
+npm -g i gatsby-cli@4.25.0
+# npm audit fix --force 2>/dev/null
+
+# for amazon linux 2022+:
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install --lts
+#npm install -g npm@9.2.0    # update npm version, could be replaced
+#npm install -g gatsby-cli
+# gatsby --version
+
 
 # Wordpress
 curl -O --output-dir /tmp https://wordpress.org/latest.tar.gz
@@ -62,27 +75,25 @@ systemctl restart httpd.service && systemctl restart php-fpm
 
 
 ####################
-# config wordpress #
-####################
+# configuring ######
+# wordpress config #
+mkdir /etc/httpd/sites-available /etc/httpd/sites-enabled
+echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf
+mv /tmp/wordpress.conf /etc/httpd/sites-available/wordpress.conf
+cp /tmp/wp-config.php ${app_path}/wordpress/wp-config.php
 
 chown -R apache:apache ${app_path}/wordpress
 find ${app_path}/wordpress/ -type d -exec chmod 750 {} \;
 find ${app_path}/wordpress/ -type f -exec chmod 640 {} \;
 
-mkdir /etc/httpd/sites-available /etc/httpd/sites-enabled
-echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf
-mv /tmp/wordpress.conf /etc/httpd/sites-available/wordpress.conf    # TODO create a file or Import
-cp /tmp/wp-config.php ${app_path}/wordpress/wp-config.php           # TODO create a file or Import
-
 ln -s /etc/httpd/sites-available/wordpress.conf /etc/httpd/sites-enabled/wordpress.conf
 
-setsebool -P httpd_unified 1
-
+# setsebool -P httpd_unified 1
 systemctl restart httpd.service && systemctl restart php-fpm
 
 
-###################
-#  config gatsby  #
-###################
-
-
+# Gatsby config
+gatsby new my-hello-world-starter https://github.com/gatsbyjs/gatsby-starter-hello-world
+cd my-hello-world-starter/
+gatsby build
+gatsby serve --host 0.0.0.0 &
